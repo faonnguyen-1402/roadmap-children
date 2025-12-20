7ff99b85bf694f2904664506930bcb65b1b6c895
Your Name <haopham0214@gmail.com>
2025-12-20 22:57:58 +0700

    chor

--- src/components/adventure2d/engine/rewards.ts
+++ src/components/adventure2d/engine/rewards.ts
@@ -8,32 +8,21 @@ export type Reward = Partial<{
 }>;
 
 export type PlayerRef = {
+  pos: { x: number; y: number };
   hp: number;
   maxHp: number;
   atk: number;
   iq: number;
-  coin: number;
   empathy: number;
 };
 export function applyReward(player: PlayerRef, reward?: Reward) {
   if (!reward) return { coinDelta: 0 };
 
-  if (reward.atk !== undefined) {
-    player.atk += reward.atk;
-  }
+  if (reward.atk !== undefined) player.atk += reward.atk;
+  if (reward.iq !== undefined) player.iq += reward.iq;
+  if (reward.empathy !== undefined) player.empathy += reward.empathy;
 
-  if (reward.iq !== undefined) {
-    player.iq += reward.iq;
-  }
-
-  if (reward.empathy !== undefined) {
-    player.empathy += reward.empathy;
-  }
-
-  if (reward.maxHp !== undefined) {
-    player.maxHp += reward.maxHp;
-  }
-
+  if (reward.maxHp !== undefined) player.maxHp += reward.maxHp;
   if (reward.hp !== undefined) {
     player.hp = Math.min(player.maxHp, player.hp + reward.hp);
   }
